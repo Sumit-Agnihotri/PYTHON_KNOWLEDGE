8b1749b8e8a7b416d13704f233683948c0b81e51
“[Sumit <sagnihotri9710@gmail.com>
2025-04-11 21:40:13 +0530

    Knowledge

---
+++ PYTHON BASICS/1 Introduction.py
@@ -0,0 +1,18 @@
+# Print Hello World
+print("Hello World")
+
+text = "Hello World"
+print(f"{text}") # f-string formatting
+
+if text == "Hello World":
+    print("The text is Hello World") # This statement has indentation.
+else:
+    print("The text is not Hello World") # This statement has indentation.
+    
+# This is a Single-Line Comment
+
+'''
+This is a Multi-Line Comment
+This is a Multi-Line Comment
+This is a Multi-Line Comment
+'''
