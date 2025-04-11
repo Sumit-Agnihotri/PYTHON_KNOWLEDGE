cb6517bc448ec877181cbb5641343561a1a31be8
“[Sumit <sagnihotri9710@gmail.com>
2025-04-11 23:12:28 +0530

    Knowledge

--- PYTHON BASICS/2 Input and Output in Python.py
+++ PYTHON BASICS/2 Input and Output in Python.py
@@ -1,2 +1,13 @@
+# Input and Output in Python
+na = input("Enter your name: ")
+print(na)
+
+# Printing Variables
+name1 = "Sumit"
+age = 20
+print("My name is", name1, "and my age is", age)
+
+# Take Multiple Input in Python
 name = input("Enter your name: ")
-print(name)
+age = int(input("Enter your age: "))
+print("My name is", name, "and my age is", age)
