cab31f2c41418d35ad8af70c36df3050459c856e
“[Sumit <sagnihotri9710@gmail.com>
2025-04-11 21:34:23 +0530

    Knowledge

---
+++ PYTHON BASICS/4 Input and Output.py
@@ -0,0 +1,19 @@
+# Taking the input from the user
+name = input("Enter your name: ")
+print("Hello, " + name + "! Welcome")
+
+# Printing the output using print() function
+print("Hello, World!")
+
+# Printing Variables
+age = 20
+print("My age is", age)
+
+# Taking Multiple Inputs from the user
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+print("The sum of", a, "and", b, "is", int(a) + int(b))
+
+# Change the type of Input
+color = input("Enter your Favourite Color: ")
+print(color)
