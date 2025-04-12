5ba4e8007154d0c9b26ec94dd0978b8ba3998761
“[Sumit <sagnihotri9710@gmail.com>
2025-04-12 22:41:05 +0530

    Knowledge

--- PYTHON BASICS/2 Input and Output in Python.py
+++ PYTHON BASICS/2 Input and Output in Python.py
@@ -11,3 +11,10 @@ print("My name is", name1, "and my age is", age)
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 print("My name is", name, "and my age is", age)
+
+# Take Conditional Input from user in Python
+age = int(input("Enter your age: "))
+if age > 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
