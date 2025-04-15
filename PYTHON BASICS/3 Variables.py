bc682ae59119aaf6e17416b0a35a0fc15497cdb4
“[Sumit <sagnihotri9710@gmail.com>
2025-04-15 15:17:27 +0530

    Knowledge

---
+++ PYTHON BASICS/3 Variables.py
@@ -0,0 +1,14 @@
+x = 5
+print(x)
+
+y = "Sumit"
+print(y)
+
+rex = 3.14 # Valid Variable Name
+_rex = 3.14 # Valid Variable Name
+as_rex = 3.14 # Valid Variable Name
+as_rex_as = 3.14 # Valid Variable Name
+
+# 12rex = 3.14 # Invalid Variable Name
+# rex@ = 3.14 # Invalid Variable Name
+
