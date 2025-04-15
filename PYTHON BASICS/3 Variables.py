5322e8224ab31c6e4f5855b96e49bc2dc489bc00
“[Sumit <sagnihotri9710@gmail.com>
2025-04-15 15:19:53 +0530

    Knowledge

--- PYTHON BASICS/3 Variables.py
+++ PYTHON BASICS/3 Variables.py
@@ -12,3 +12,15 @@ as_rex_as = 3.14 # Valid Variable Name
 # 12rex = 3.14 # Invalid Variable Name
 # rex@ = 3.14 # Invalid Variable Name
 
+a = b = c = 1 # Multiple Assignment
+print(a, b, c)
+a, b, c = 1, 2, 3 # Multiple Assignment
+print(a, b, c)
+
+# TYPE CASTING
+x = 5
+y = 2.5
+z = "10"
+print(str(x))
+print(int(y))
+print(float(z))
